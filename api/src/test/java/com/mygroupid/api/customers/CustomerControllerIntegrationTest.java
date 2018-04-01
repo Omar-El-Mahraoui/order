70a0b1c1a92d819257c683df3b9ec6a1c49a8e70
El Mahraoui Omar (100) <omar.elmahraoui@cmc.be>
2018-04-01 12:42:42 +0200

    create customer controller test seccuessfull

--- api/src/test/java/com/mygroupid/api/customers/CustomerControllerIntegrationTest.java
+++ api/src/test/java/com/mygroupid/api/customers/CustomerControllerIntegrationTest.java
@@ -1,17 +1,7 @@
-/*
 package com.mygroupid.api.customers;
 
 
-import com.mygroupid.api.orders.ItemGroupDto;
 import com.mygroupid.api.orders.ItemGroupMapper;
-import com.mygroupid.api.orders.OrderDto;
-import com.mygroupid.domain.customers.Customer;
-import com.mygroupid.domain.customers.CustomerDatabase;
-import com.mygroupid.domain.items.Item;
-import com.mygroupid.domain.items.ItemDatabase;
-import com.mygroupid.domain.orders.ItemGroup;
-import com.mygroupid.domain.orders.Order;
-import com.mygroupid.domain.orders.OrderDatabase;
 import com.mygroupid.service.customers.CustomerService;
 import com.mygroupid.service.items.ItemService;
 import com.mygroupid.service.orders.OrderService;
@@ -22,16 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.inject.Inject;
 
-import java.util.UUID;
-
+import static com.mygroupid.api.customers.CustomerDto.customerDto;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.SpringApplication.run;
-// copied and adapted code from funiversity
+// copied and adapted code from funiversity switchfully
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CustomerControllerIntegrationTest.CustomerControllerIntegrationTestRunner.class
@@ -67,18 +55,7 @@ public class CustomerControllerIntegrationTest {
         customerService.clearDatabase();
     }
 
-    private CustomerDto createCustomerDto() {
-        CustomerDto customerDto = new CustomerDto();
-        customerDto.setId(UUID.randomUUID().toString());
-        customerDto.setFirstName("Jan");
-        customerDto.setLastName("Janssens");
-        customerDto.setEmailAddress("jansemailaddress@exampleameil.com");
-        customerDto.setAddress("jansaddress");
-        customerDto.setPhoneNumber("0123456789");
-        return customerDto;
-    }
-
-    private ItemGroupDto createItemGroupDto() {
+    /*private ItemGroupDto createItemGroupDto() {
         Item item = new Item();
         item.setName("pen");
         item.setDescription("blue pen");
@@ -89,12 +66,17 @@ public class CustomerControllerIntegrationTest {
         itemGroupDto.setItemId(item.getId());
         itemGroupDto.setAmount("4");
         return itemGroupDto;
-    }
+    }*/
 
     @Test
-    public void createCustomer_givenAnEmptyDatabaseAndACustomerDto_thenAddCustomerToCustomerDatabaseAnd(){
+    public void createCustomer_givenAnEmptyDatabaseAndACustomerDto_thenAddCustomerToCustomerDatabaseAndReturnCustomerDto(){
         // given
-        CustomerDto customerDtoGiven = createCustomerDto();
+        CustomerDto customerDtoGiven = customerDto()
+                .withFirstName("Jan")
+                .withLastName("Janssens")
+                .withEmailAddress("jansemailaddress@exampleemail.com")
+                .withAddress("jansaddress")
+                .withPhoneNumber("0123456789");
 
         //when
         CustomerDto customerDtoReturned = new TestRestTemplate()
@@ -104,13 +86,14 @@ public class CustomerControllerIntegrationTest {
 
         //then
         assertThat(customerService.getCustomers()).hasSize(1);
+        assertThat(customerDtoReturned.getId()).isNotNull();
         assertThat(customerService.getCustomers().get(0).getFirstName()).isEqualTo(customerDtoGiven.getFirstName());
         assertThat(customerService.getCustomers().get(0).getLastName()).isEqualTo(customerDtoGiven.getLastName());
         assertThat(customerService.getCustomers().get(0).getEmailAddress()).isEqualTo(customerDtoGiven.getEmailAddress());
         assertThat(customerService.getCustomers().get(0).getAddress()).isEqualTo(customerDtoGiven.getAddress());
         assertThat(customerService.getCustomers().get(0).getPhoneNumber()).isEqualTo(customerDtoGiven.getPhoneNumber());
     }
-
+/*
     @Test
     public void createOrder_givenACustomerIdAndAItemGroupDto_thenCreateOrderAndAddItToOrderDatabseAndUpdateItem(){
         // given
@@ -136,10 +119,9 @@ public class CustomerControllerIntegrationTest {
         ItemGroupDto itemGroupDto = itemGroupMapper.toDto(itemGroup);
 
 
-        */
-/*CustomerDto customerDtoGiven = createCustomerDto();
+        CustomerDto customerDtoGiven = createCustomerDto();
         String customerId = customerDtoGiven.getId();
-        ItemGroupDto itemGroupDto = createItemGroupDto();*//*
+        ItemGroupDto itemGroupDto = createItemGroupDto();
 
 
         //when
@@ -150,15 +132,12 @@ public class CustomerControllerIntegrationTest {
 
         //then
         assertThat(orderService.getOrders()).hasSize(1);
-        */
-/*assertThat(customerService.getCustomers().get(0).getFirstName()).isEqualTo(customerDtoGiven.getFirstName());
+assertThat(customerService.getCustomers().get(0).getFirstName()).isEqualTo(customerDtoGiven.getFirstName());
         assertThat(customerService.getCustomers().get(0).getLastName()).isEqualTo(customerDtoGiven.getLastName());
-        assertThat(customerService.getCustomers().get(0).getEmailAddress()).isEqualTo(customerDtoGiven.getEmailAddress());*//*
+        assertThat(customerService.getCustomers().get(0).getEmailAddress()).isEqualTo(customerDtoGiven.getEmailAddress());
 
     }
 
-    */
-/*
     @Test
     public void getCustomers_givenAnEmptyDatabase_thenReturnAnEmptyArrayList(){
         // given
@@ -190,7 +169,6 @@ public class CustomerControllerIntegrationTest {
         assertThat(customerService.getCustomers().get(0).getPhoneNumber()).isEqualTo(customerDtoGiven.getPhoneNumber());
 
     }
-    *//*
 
-}
 */
+}
