c459d25465c0ec0ccd790e5dbfdcb1c20b88ff4b
El Mahraoui Omar (100) <omar.elmahraoui@cmc.be>
2018-04-01 18:37:10 +0200

    added test for creating item in itemserviceintegrationtest

--- service/src/test/java/com/mygroupid/service/items/ItemServiceIntegrationTest.java
+++ service/src/test/java/com/mygroupid/service/items/ItemServiceIntegrationTest.java
@@ -5,7 +5,6 @@ import com.mygroupid.domain.items.ItemDatabase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
